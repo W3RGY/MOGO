502786bf5a9a5c907e0eafa663e07363b8f6598b
W3RGY <w3rgy1992@gmail.com>
2022-08-08 20:49:08 +0200

    main

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ $(function () {
 		header = $("#header"),
 		scrollOffSet = $(window).scrollTop();
 
+	/* Fixed Header */
 		checkScroll(scrollOffSet);
 
 	$(window).on("scroll", function () {
@@ -21,4 +22,31 @@ $(function () {
 			header.removeClass("fixed");
 		}
 	};
+
+	/* Smooth scroll */
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		var $this = $(this),
+			blockId = $this.data('scroll');
+			blockOffSet = $(blockId).offset().top;
+
+		$("#nav a").removeClass("active");
+		$(this).addClass("active");
+
+		$("html, body").animate({
+			scrollTop: blockOffSet
+		}, 500);
+	});
+
+
+	/* Menu nav toggle */
+	$("#nav_toggle").on("click", function (event) {
+		event.preventDefault();
+
+		$(this).toggleClass("active");
+		$("#nav").toggleClass("active");
+	})
+
+
 });
